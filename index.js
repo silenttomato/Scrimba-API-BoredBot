6b4404fa95b0537ee1f0ebb5643c725f0806058f
webthetrees <111392356+webthetrees@users.noreply.github.com>
2025-01-07 16:38:08 +0100

    eod commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('energyButton').addEventListener('click', function() {
+    document.body.classList.add('vibrant-background');
+    document.getElementById('title').textContent = '😎 BoredBot 😎';
+
+});
