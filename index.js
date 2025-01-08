c5ede20c7bd2c29fc7f59a42f635298375556063
webthetrees <111392356+webthetrees@users.noreply.github.com>
2025-01-08 10:08:32 +0100

    final fixes, added aria label, dynmic titles. fun

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-document.getElementById('energyButton').addEventListener('click', function() {
+document.getElementById('get-activity').addEventListener('click', function() {
+    document.title = 'ActiveBot by mox';
     document.body.classList.add('vibrant-background');
-    document.getElementById('title').textContent = '😎 BoredBot 😎';
+    document.getElementById('header').textContent = '😎 ActiveBot 😎';
+    document.getElementById('header').setAttribute('aria-label', 'ActiveBot');
 
+    fetch("https://apis.scrimba.com/bored/api/activity")
+        .then(response => response.json())
+        .then(data => document.getElementById('activity').textContent = data.activity)
 });
